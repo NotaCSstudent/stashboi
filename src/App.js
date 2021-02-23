f5a1eca50e2eed23f76322103f17af36ccf2593b
mahamud babu <mahmudbabu94@gmail.com>
2021-02-23 15:49:43 -0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./App.css"
+export default class App extends Component {
+  render() {
+    return (
+      <bg class="background">
+      <div class ="background">
+        hello world
+        <h1 class="name">STONKSTASH</h1>
+      <h1 class = "name1">
+        
+        Welcome To Stonkstash
+      </h1>
+        <div class="firstphone"></div>
+        <h1 class= "detail1">
+        The only tool you need to become a successful stock market investor.
+          </h1>
+      </div>
+      </bg>
+    )
+  }
+}
