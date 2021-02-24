cdb981507c2b2461e9f7323c6c6fa78522629ea9
mahamud babu <mahmudbabu94@gmail.com>
2021-02-24 13:33:11 -0500

    some more commits

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export default class App extends Component {
       <bg class="background">
       <div class ="background">
         hello world
-        <h1 class="name">STONKSTASH</h1>
+        <h1 class="name">Stonkstash</h1>
+        <div class="containerstasche">
+         
+        
+        
+        </div>
+      <div class="Retanglearoundphone"></div>
       <h1 class = "name1">
         
         Welcome To Stonkstash
@@ -14,7 +20,22 @@ export default class App extends Component {
         <div class="firstphone"></div>
         <h1 class= "detail1">
         The only tool you need to become a successful stock market investor.
+        
           </h1>
+          <div class="missionstatement"><h1 class="missiondetail">Our mission is to help YOU use the free investing resources of today to your benefit rather than your detrimate.</h1></div>
+          
+          <button class="informbutton">
+            <font face="Coda"><font color="White"><font weight="1"><font align="Center">
+              <font size="5">
+                <font color="94CFC1">
+                Inform Me!
+                </font>
+              </font>
+            </font>
+
+             </font>
+              </font></font></button>
+      
       </div>
       </bg>
     )
--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,1 +1,23 @@
 
+import React, { Component } from 'react'
+
+export default class landingpage extends Component {
+    render() {
+        return (
+           
+                   <div class ="background">
+        hello world
+        <h1 class="name">STONKSTASH</h1>
+      <h1 class = "name1">
+        
+        Welcome To Stonkstash
+      </h1>
+        <div class="firstphone"></div>
+        <h1 class= "detail1">
+        The only tool you need to become a successful stock market investor.
+          </h1>
+      </div>  
+            
+        )
+    }
+}
